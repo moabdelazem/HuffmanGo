b97779b057e8e021599e1b7f5dd0ac728e24ab4b
Mohamed Abdelazim <mabdelazemahmed@gmail.com>
2024-08-04 21:08:58 +0300

    fix: impelmenting the priority queue

--- huffman/pq.go
+++ huffman/pq.go
@@ -1,15 +1,32 @@
 package huffman
 
-type PriorityQueue []*Node
+type PriorityQueue []*HeapNode
 
 func (pq PriorityQueue) Len() int {
 	return len(pq)
 }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-	return i < j
+	return pq[i].Freq < pq[j].Freq
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	newItem := x.(*HeapNode)
+	newItem.Index = len(*pq)
+	*pq = append(*pq, newItem)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	oldPQ := *pq
+	n := len(oldPQ)
+	item := oldPQ[n-1]
+	item.Index = -1
+	*pq = oldPQ[0 : n-1]
+	return item
 }
